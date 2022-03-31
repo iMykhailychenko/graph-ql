d119d5939115551186f7ae6fbf3cedf7a28afb4f
imykhailychenko <igor.c.m@ukr.net>
2022-03-31 13:45:13 +0200

    init

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 
-const { PORT, MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD } =
-  process.env;
+const {
+  PORT,
+  MONGODB_URI,
+  MONGO_INITDB_ROOT_USERNAME,
+  MONGO_INITDB_ROOT_PASSWORD,
+} = process.env;
 
 const port = PORT || 3000;
 const isMongoVarVisible = Boolean(
   MONGO_INITDB_ROOT_USERNAME && MONGO_INITDB_ROOT_PASSWORD
 );
 
-const mongoUrl = "mongodb://localhost:27017";
+const mongoUrl = MONGODB_URI;
 const mongoUser = MONGO_INITDB_ROOT_USERNAME;
 const mongoPass = MONGO_INITDB_ROOT_PASSWORD;
 const mongoDbName = "graphql";
