67ad5a8030faa2951128554bc791fd0229c75557
imykhailychenko <igor.c.m@ukr.net>
2022-04-01 16:54:30 +0200

    update hostname

--- blog/graphql/resolvers.js
+++ blog/graphql/resolvers.js
@@ -1,7 +1,7 @@
 const { PostModel } = require("../api/posts/posts.model");
 
 const Query = {
-  post: async (root, { id }) => await PostModel.findOne(id),
+  post: async (root, { id }) => await PostModel.findOne({ id }),
   posts: async () => await PostModel.find(),
 };
 
